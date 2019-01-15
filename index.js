2c6a621fd75fccffc614e072f948a7e8fb1dabc8
vagrant <rmdwirizki@gmail.com>
2019-01-15 07:20:10 +0700

    Ganti jadi kapital

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 "use strict"
 
 var convertToUnit = function (digit) {
-  if (digit >= 63) return 'vigintiliun'
-  else if (digit >= 60) return 'novemdeciliun'
-  else if (digit >= 57) return 'octodeciliun'
-  else if (digit >= 54) return 'septendeciliun'
-  else if (digit >= 51) return 'sexdeciliun'
-  else if (digit >= 48) return 'quindeciliun'
-  else if (digit >= 45) return 'quattuordeciliun'
-  else if (digit >= 42) return 'tredeciliun'
-  else if (digit >= 39) return 'duodeciliun'
-  else if (digit >= 36) return 'undeciliun'
-  else if (digit >= 33) return 'deciliun'
-  else if (digit >= 30) return 'noniliun'
-  else if (digit >= 27) return 'octiliun'
-  else if (digit >= 24) return 'septiliun'
-  else if (digit >= 21) return 'sextiliun'
-  else if (digit >= 18) return 'quintiliun'
-  else if (digit >= 15) return 'quadriliun'
-  else if (digit >= 12) return 'triliun'
-  else if (digit >= 9) return 'milyar'
-  else if (digit >= 6) return 'juta'
-  else if (digit >= 3) return 'ribu'
+  if (digit >= 63) return 'Vigintiliun'
+  else if (digit >= 60) return 'Novemdesiliun'
+  else if (digit >= 57) return 'Octodesiliun'
+  else if (digit >= 54) return 'Septendesiliun'
+  else if (digit >= 51) return 'Sexdesiliun'
+  else if (digit >= 48) return 'Quindesiliun'
+  else if (digit >= 45) return 'Quattuordesiliun'
+  else if (digit >= 42) return 'Tredesiliun'
+  else if (digit >= 39) return 'Duodesiliun'
+  else if (digit >= 36) return 'Undesiliun'
+  else if (digit >= 33) return 'Desiliun'
+  else if (digit >= 30) return 'Noniliun'
+  else if (digit >= 27) return 'Octiliun'
+  else if (digit >= 24) return 'Septiliun'
+  else if (digit >= 21) return 'Sextiliun'
+  else if (digit >= 18) return 'Quintiliun'
+  else if (digit >= 15) return 'Quadriliun'
+  else if (digit >= 12) return 'Triliun'
+  else if (digit >= 9) return 'Milyar'
+  else if (digit >= 6) return 'Juta'
+  else if (digit >= 3) return 'Ribu'
   else return ''
 }
 
-var numbers = ['satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan']
+var numbers = ['Satu', 'Dua', 'Tiga', 'Empat', 'Lima', 'Enam', 'Tujuh', 'Delapan', 'Sembilan']
 var numberToString = function (index) {
   return numbers[index - 1] || ''
 }
@@ -38,7 +38,7 @@ module.exports = function terbilang(angka) {
   for (var i = 0; i < angka.length; i++) {
     var length = angka.length - 1 - i
     if (length % 3 == 0) {
-      var num = (angka[i] == 1 && (isBelasan || (convertToUnit(length) == 'ribu' && ((angka[i - 2] == undefined || angka[i - 2] == 0) && (angka[i - 1] == undefined || angka[i - 1] == 0))))) ? 'se' : `${numberToString(angka[i])} `
+      var num = (angka[i] == 1 && (isBelasan || (convertToUnit(length) == 'Ribu' && ((angka[i - 2] == undefined || angka[i - 2] == 0) && (angka[i - 1] == undefined || angka[i - 1] == 0))))) ? 'Se' : `${numberToString(angka[i])} `
       result += ` ${num}`
 
       if ((angka[i - 2] && angka[i - 2] != 0) || (angka[i - 1] && angka[i - 1] != 0) || angka[i] != 0) {
@@ -46,22 +46,22 @@ module.exports = function terbilang(angka) {
       }
       if (printUnit) {
         printUnit = false
-        result += ((isBelasan) ? 'belas ' : '') + convertToUnit(length)
+        result += ((isBelasan) ? 'Belas ' : '') + convertToUnit(length)
         if (isBelasan) {
           isBelasan = false
         }
       }
     } else if (length % 3 == 2 && angka[i] != 0) {
-      result += ` ${(angka[i] == 1) ? 'se' : numberToString(angka[i]) + ' '}ratus`
+      result += ` ${(angka[i] == 1) ? 'Se' : numberToString(angka[i]) + ' '}Ratus`
     } else if (length % 3 == 1 && angka[i] != 0) {
       if (angka[i] == 1) {
         if (angka[i + 1] == 0) {
-          result += ' sepuluh'
+          result += ' Sepuluh'
         } else {
           isBelasan = true
         }
       } else {
-        result += ` ${numberToString(angka[i])} puluh`
+        result += ` ${numberToString(angka[i])} Puluh`
       }
     }
   }
